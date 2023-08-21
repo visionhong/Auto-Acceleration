2e4185e81bce674c056472b5268159c4f9735032
EunPyo Hong <53398821+visionhong@users.noreply.github.com>
2023-08-21 23:24:48 +0900

    Update onnx2tensorrt.sh

--- export/onnx2tensorrt.sh
+++ export/onnx2tensorrt.sh
@@ -9,19 +9,19 @@ IO16=$6
 DYNAMIC=$7
 
 if [ ${DYNAMIC} = "False" ]; then
-    trtexec --onnx=$MODEL --saveEngine=${OUTPUT_PATH}_fp32.trt 
-    trtexec --onnx=$MODEL --saveEngine=${OUTPUT_PATH}_fp16.trt --fp16 
+    trtexec --onnx=$MODEL --saveEngine=${OUTPUT_PATH}_fp32.plan 
+    trtexec --onnx=$MODEL --saveEngine=${OUTPUT_PATH}_fp16.plan --fp16 
 
     if [ ${IO16} = "True" ]; then
-        trtexec --onnx=$MODEL --saveEngine=${OUTPUT_PATH}_fp32_io16.trt --inputIOFormats=fp16:chw --outputIOFormats=fp16:chw 
-        trtexec --onnx=$MODEL --saveEngine=${OUTPUT_PATH}_fp16_io16.trt --inputIOFormats=fp16:chw --outputIOFormats=fp16:chw --fp16
+        trtexec --onnx=$MODEL --saveEngine=${OUTPUT_PATH}_fp32_io16.plan --inputIOFormats=fp16:chw --outputIOFormats=fp16:chw 
+        trtexec --onnx=$MODEL --saveEngine=${OUTPUT_PATH}_fp16_io16.plan --inputIOFormats=fp16:chw --outputIOFormats=fp16:chw --fp16
     fi
 else
-    trtexec --onnx=$MODEL --saveEngine=${OUTPUT_PATH}_fp32.trt --minShapes=$MIN_SHAPE --optShapes=$OPT_SHAPE --maxShapes=$MAX_SHAPE
-    trtexec --onnx=$MODEL --saveEngine=${OUTPUT_PATH}_fp16.trt --minShapes=$MIN_SHAPE --optShapes=$OPT_SHAPE --maxShapes=$MAX_SHAPE --fp16
+    trtexec --onnx=$MODEL --saveEngine=${OUTPUT_PATH}_fp32.plan --minShapes=$MIN_SHAPE --optShapes=$OPT_SHAPE --maxShapes=$MAX_SHAPE
+    trtexec --onnx=$MODEL --saveEngine=${OUTPUT_PATH}_fp16.plan --minShapes=$MIN_SHAPE --optShapes=$OPT_SHAPE --maxShapes=$MAX_SHAPE --fp16
 
     if [ ${IO16} = "True" ]; then
-        trtexec --onnx=$MODEL --saveEngine=${OUTPUT_PATH}_fp32_io16.trt --inputIOFormats=fp16:chw --outputIOFormats=fp16:chw --minShapes=$MIN_SHAPE --optShapes=$OPT_SHAPE --maxShapes=$MAX_SHAPE
-        trtexec --onnx=$MODEL --saveEngine=${OUTPUT_PATH}_fp16_io16.trt --inputIOFormats=fp16:chw --outputIOFormats=fp16:chw --fp16 --minShapes=$MIN_SHAPE --optShapes=$OPT_SHAPE --maxShapes=$MAX_SHAPE
+        trtexec --onnx=$MODEL --saveEngine=${OUTPUT_PATH}_fp32_io16.plan --inputIOFormats=fp16:chw --outputIOFormats=fp16:chw --minShapes=$MIN_SHAPE --optShapes=$OPT_SHAPE --maxShapes=$MAX_SHAPE
+        trtexec --onnx=$MODEL --saveEngine=${OUTPUT_PATH}_fp16_io16.plan --inputIOFormats=fp16:chw --outputIOFormats=fp16:chw --fp16 --minShapes=$MIN_SHAPE --optShapes=$OPT_SHAPE --maxShapes=$MAX_SHAPE
     fi
 fi
